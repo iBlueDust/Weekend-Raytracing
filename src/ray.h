daa787ec54edf0e0e73b166974afa4c88a79de86
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2022-12-19 21:29:04 +0800

    4.1 The Ray Class

---
+++ src/ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "vec3.h"
+
+class Ray {
+public:
+	// Using linear algebra representation
+	// r = a + bt
+	point3 origin;
+	vec3 direction; // author explicitly chose against making this a unit vector
+
+	ray() {}
+	ray(const point3& origin, const vec3& direction)
+		: origin(origin), direction(direction) {}
+
+	point3 at(double t) const {
+		return origin + t * direction;
+	}
+};
