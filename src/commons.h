59928e4d4a84105281685be18e78a3ec94265247
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2022-12-21 11:07:55 +0800

    7.1 Some Random Number Utilities

---
+++ src/commons.h
@@ -0,0 +1,9 @@
+#pragma random
+
+#include <random>
+
+inline double randomDouble() {
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
