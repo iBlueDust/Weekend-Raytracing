60bf66dbc8b59e799e8a5da13ef9b3a0f59103ee
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2022-12-23 00:19:11 +0800

    refactor: add objects in unison

--- src/main.cpp
+++ src/main.cpp
@@ -83,12 +83,13 @@ int main(int argc, char** argv) {
 	auto materialRight = std::make_shared<metal>(color3(0.8, 0.6, 0.2), 0.0);
 
 
-	world.add(std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround));
-	world.add(std::make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter));
-	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-	world.add(std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, materialLeft));
-	world.add(std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, materialRight));
-
+	world.addMany({
+		std::make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, materialGround),
+		std::make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, materialCenter),
+		std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, materialLeft),
+		std::make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, materialLeft),
+		std::make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, materialRight)
+	});
 
 
 	// Camera
