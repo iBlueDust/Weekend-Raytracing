3e90d1b33c00ac8fe9aac1a3e8397551d91f3ed8
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2023-01-08 03:04:39 +0800

    fix: color overflow
    Turns very bright pixels completely black for some reason.

--- src/color.h
+++ src/color.h
@@ -7,6 +7,10 @@
 #include <iostream>
 
 void writePixel(std::ostream& stream, color3 pixel) {
+	pixel.r = std::clamp(pixel.r, 0.0, 1.0);
+	pixel.g = std::clamp(pixel.g, 0.0, 1.0);
+	pixel.b = std::clamp(pixel.b, 0.0, 1.0);
+
 	stream << static_cast<int>(255.999 * pixel.x) << ' '
 	       << static_cast<int>(255.999 * pixel.y) << ' '
 	       << static_cast<int>(255.999 * pixel.z) << '\n';
