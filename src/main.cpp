d4f94ee8e8fcebadbe48f67815f15ba8460b2238
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2023-01-08 02:46:24 +0800

    fix: run threads with different rng seeds

--- src/main.cpp
+++ src/main.cpp
@@ -160,7 +160,7 @@ int main(int argc, char** argv) {
 				imageWidth,
 				imageHeight,
 				threadSampleCount,
-				seed,
+				seed + i,
 				maxBounces,
 				std::ref(world),
 				std::ref(mainCamera),
