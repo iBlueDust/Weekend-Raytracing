49dbf83b6e68abfec2792c423d6183c843dad820
iBlueDust <35223375+iBlueDust@users.noreply.github.com>
2023-01-07 22:33:36 +0800

    feat: complete cornell_box_scene

--- src/scene.h
+++ src/scene.h
@@ -137,25 +137,25 @@ public:
 	virtual hittable_list build() override {
 		hittable_list world;
 
-		world.add(
+		world.addMany({
 			std::make_shared<mesh>(
 				std::initializer_list<point3>{
 					point3(-1, -1, -2),    // 0
 					point3(-1, -1, 1),     // 1
 					point3(1, -1, 1),      // 2
 					point3(1, -1, -2),     // 3
-										   
+
 					point3(-1, 1, -2),     // 4
 					point3(-1, 1, 1),      // 5
 					point3(1, 1, -2),      // 6
 					point3(1, 1, 1),       // 7
-										   
+
 					point3(-0.5, 1, -0.5), // 8
 					point3(-0.5, 1, 0.5),  // 9
 					point3(0.5, 1, -0.5),  // 10
 					point3(0.5, 1, 0.5)    // 11
 				},
-				std::initializer_list<int>{ 
+				std::initializer_list<int>{
 					0, 1, 2, 0, 2, 3, // bottom face
 					2, 3, 7, 3, 7, 6, // left face
 					5, 4, 1, 1, 0, 4, // right face
@@ -169,9 +169,9 @@ public:
 					4, 6, 8, 8, 6, 10    // back eave 
 				},
 				std::initializer_list<std::shared_ptr<material>> {
-					std::make_shared<lambertian_diffuse>(color3(0.9)),
-					std::make_shared<lambertian_diffuse>(color3(0.9, 0, 0)),
-					std::make_shared<lambertian_diffuse>(color3(0, 0.9, 0))
+					std::make_shared<lambertian_diffuse>(color3(1)),
+					std::make_shared<lambertian_diffuse>(color3(1, 0, 0)),
+					std::make_shared<lambertian_diffuse>(color3(0, 1, 0))
 					},
 				std::initializer_list<int> {
 					0, 0, // bottom face
@@ -179,8 +179,23 @@ public:
 					2, 2, // right face
 					0 // everything else
 				}
-			)
-		);
+			),
+			std::make_shared<sphere>(
+				point3(-0.5, -0.65, 0.1),
+				0.35,
+				std::make_shared<metal>(color3(0, 0.2, 0.8), 0.8)
+			),
+			std::make_shared<sphere>(
+				point3(0.4, -0.5, 0.3),
+				0.5,
+				std::make_shared<lambertian_diffuse>(color3(0.4, 0.1, 0))
+			),
+			std::make_shared<sphere>(
+				point3(0, -0.6, -0.2),
+				0.4,
+				std::make_shared<dielectric>(1.250)
+			),
+		});
 
 		return world;
 	}
